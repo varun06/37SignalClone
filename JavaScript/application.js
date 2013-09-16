386c8206059b904d0d8cb0f5a46637b4e960d741
Varun <varunkuamr.saini06@gmail.com>
2013-09-16 06:56:18 -0500

    Modified Indentation

--- JavaScript/application.js
+++ JavaScript/application.js
@@ -1,7 +1,11 @@
+// JQuery to implement the hover effect
+
+// Hide product_desc div on document load
 $(document).ready(function(){
      $('.product_desc').hide();
 });
 
+// Show the divs to hover
 $(document).ready(function(){
     $('.hover_bc').hover(function(){
         $('#baseCamp_desc').toggle();
