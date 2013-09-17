23a7e74c4f16f33f8b9543903d4fcc32a96096bb
Varun <varunkuamr.saini06@gmail.com>
2013-09-17 06:16:35 -0500

    JQuery Updates

--- JavaScript/application.js
+++ JavaScript/application.js
@@ -1,24 +1,22 @@
 // JQuery to implement the hover effect
 
 // Hide product_desc div on document load
-$(document).ready(function(){
+$(function(){
      $('.product_desc').hide();
-});
 
 // Show the divs to hover
-$(document).ready(function(){
     $('.hover_bc').hover(function(){
         $('#baseCamp_desc').toggle();
-        $('#mainDesc').toggle()
-     });
+        $('#mainDesc').toggle();
+    });
 
     $('.hover_hr').hover(function() {
         $('#highRise_desc').toggle();
-        $('#mainDesc').toggle()
+        $('#mainDesc').toggle();
      });
 
     $('.hover_cf').hover(function(){
        $('#campFire_desc').toggle();
-       $('#mainDesc').toggle()
+       $('#mainDesc').toggle();
      });
 });
